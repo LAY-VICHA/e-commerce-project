5f742ad11bb69219ffaca4ef194f9e39df16ee18
lay_vicha <layvicha081783923@gmail.com>
2023-06-23 22:59:22 +0700

    chage project name

---
+++ e-commerce-project/README.md
@@ -0,0 +1,34 @@
+# What i have done
+Landing page with  route /
+Home page with route /home
+list of shampoo page with route /shampoo
+
+# e-commerce-homepage
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ e-commerce-project/src/views/HeaderView.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+<template>
+  <header class="header">
+    <div class="header-component">
+      <div>
+        <i class="fa-solid fa-bars"></i>
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </div>
+      <div class="store-name">
+        Shampoo Store
+      </div>
+      <div>
+        <i class="fa-solid fa-user"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+      </div>
+    </div>
+    <div class="header-nav">
+      <ul class="header-nav-ulist">
+        <router-link class="router-link" to="/home">
+          <li class="nav-list">Home</li>
+        </router-link>
+        
+        <router-link class="router-link" to="/shampoo">
+          <li class="nav-list">Shampoo</li>
+        </router-link>
+        <li class="nav-list">Category2</li>
+        <li class="nav-list">About us</li>
+      </ul>
+    </div>
+  </header>
+</template>
